d58e98a50ea67ad91fec6331bc2ab2fbb244e312
Michiel <michiel.cnossen@nedap.com>
2022-03-07 10:51:48 +0100

    Added data print functions to com_read and com_write for debugging

--- com.c
+++ com.c
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
+#include "log.h"
 
 #ifdef __MINGW32__
 static HANDLE hSerial;
@@ -19,6 +21,15 @@ static HANDLE hSerial;
 #ifdef __linux
 static int fd;
 #endif
+
+char *array_to_str(char * str, uint8_t *array, unsigned int n) {
+  int r;
+  if (n == 0) return 0;
+  if (n == 1) r = sprintf(str, "%X", array[0]);
+  else        r = sprintf(str, "%X, ", array[0]);
+  array_to_str(str + r, array + 1, n - 1);
+  return str;
+}
 
 static uint32_t COM_Baudrate = 115200;
 
@@ -129,6 +140,8 @@ bool COM_Open(char *port, uint32_t baudrate, bool have_parity, bool two_stopbits
  */
 int COM_Write(uint8_t *data, uint16_t len)
 {
+  char buf[128];
+  LOG_Print(LOG_LEVEL_INFO, "COM_Write(%s)", array_to_str(buf, data, len));
   #ifdef __MINGW32__
   DWORD dwBytesWritten = 0;
   //DWORD signal;
@@ -164,8 +177,10 @@ int COM_Write(uint8_t *data, uint16_t len)
  * \return number of received bytes as int
  *
  */
+
 int COM_Read(uint8_t *data, uint16_t len)
 {
+  char buf[128];
   #ifdef __MINGW32__
   //OVERLAPPED ov = { 0 };
   //COMSTAT status;
@@ -195,8 +210,11 @@ int COM_Read(uint8_t *data, uint16_t len)
   #ifdef __linux
   int dwBytesRead = read(fd, data, len);
   if (dwBytesRead < 0)
+    LOG_Print(LOG_LEVEL_INFO, "COM_Read(-1)");
     return -1;
   #endif
+
+  LOG_Print(LOG_LEVEL_INFO, "COM_Read(%s)", array_to_str(buf, data, len));
 
   return dwBytesRead;
 }
